6f438cc5a1f3e73240db244ae02c5822e3803512
mslysz <macslysz@gmail.com>
2023-07-02 01:25:50 +0100

    added task finder code to main.js file

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const input = document.querySelector('input');
 const taskNumber = document.querySelector('h2 span');
 const ul = document.querySelector('ul');
 const tasksList = document.getElementsByClassName('task');
+const secondInput = document.getElementById('secondInput');
 
 
 const removeTask = (e) => {
@@ -13,10 +14,10 @@ const removeTask = (e) => {
 
 const addTask = (e) => {
   e.preventDefault();
-  const titleTask = input.value
+  const titleTask = input.value;
   if (titleTask === "") return;
   const task = document.createElement('li');
-  task.className = 'task'
+  task.className = 'task';
   task.innerHTML = titleTask + '<button>Delete</button>';
   ul.appendChild(task);
   taskNumber.textContent = tasksList.length;
@@ -25,6 +26,22 @@ const addTask = (e) => {
 }
 
 
+const searchTask = (e) => {
+  const searchText = e.target.value.toLowerCase();
+  let tasks = [...tasksList];
+  if (searchText === "") {
+    tasks.forEach(task => task.style.display = 'block');
+  } else {
+    tasks.forEach(task => {
+      if (task.textContent.toLowerCase().includes(searchText)) {
+        task.style.display = "block";
+      } else {
+        task.style.display = "none"
+      }
+    })
+  }
 
+}
 
 form.addEventListener('submit', addTask)
+secondInput.addEventListener('input', searchTask)
